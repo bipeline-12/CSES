6b12926b2d761a258187371390d2fdfe335bae27
Bharath_Ram <121173266+bipeline-12@users.noreply.github.com>
2023-07-27 01:20:59 +0530

    Create P1.cpp
    no output , cuz i made a bullshit mistake of not typing 
    " cout << solve(n);"

---
+++ P1.cpp
@@ -0,0 +1,37 @@
+//Wierd Algorithm 
+
+
+#include <bits/stdc++.cpp>
+using namespace std;
+
+int solve(int n)
+{
+	
+	int m = n;
+	int count=1;
+	while (m!=1){
+
+		if(m%2 == 0){
+	    m=m/2;
+	    count++;
+	    }
+	    else{
+	        m=(3*m+1);
+	        count++;
+	    }
+	  }
+
+	  return count;
+
+}
+
+
+int main(){
+	int n;
+	cin >> n ;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	solve(n);
+	return 0;
+}
